c81453c37599af8eae795a61f25f2cd7abeae56d
Mr1ganka <103485310+Mr1ganka@users.noreply.github.com>
2026-08-16 15:33:30 +0530

    Updates

---
+++ _next/static/chunks/app/bangalore/page-ee96b2e09409be3b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[779,239,37],{3263:function(n,e,u){Promise.resolve().then(u.bind(u,1667))}},function(n){n.O(0,[702,667,971,117,744],function(){return n(n.s=3263)}),_N_E=n.O()}]);
